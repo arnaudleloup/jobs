4f6aecc3a9d8ee785b21a0d9964594684e0c61fb
arnaudleloup <leloup.arnaud@gmail.com>
2015-03-10 19:59:09 +0100

    Update of level 5 main

--- backend/level5/main.rb
+++ backend/level5/main.rb
@@ -17,25 +17,17 @@ def write(file, output)
   end
 end
 
-actions = {
-  :driver => DriverAction.new,
-  :owner => OwnerAction.new,
-  :insurance => InsuranceAction.new,
-  :assistance => AssistanceAction.new,
-  :drivy => DrivyAction.new
-}
-
 data = read("backend/level5/data.json")
 rentals = RentalsCalc.new(data).rentals
 
 output_rentals = []
 rentals.each do |rental|
   rental_actions = [
-    actions[:driver].calc(rental),
-    actions[:owner].calc(rental),
-    actions[:insurance].calc(rental),
-    actions[:assistance].calc(rental),
-    actions[:drivy].calc(rental),
+    DriverAction.new.calc(rental),
+    OwnerAction.new.calc(rental),
+    InsuranceAction.new.calc(rental),
+    AssistanceAction.new.calc(rental),
+    DrivyAction.new.calc(rental),
   ]
   output_rentals.push({
    :id => rental[:id],
@@ -48,4 +40,4 @@ output = {
   :rentals => output_rentals
 }
 
-write("backend/level5/output2.json", output)
+write("backend/level5/output.json", output)
